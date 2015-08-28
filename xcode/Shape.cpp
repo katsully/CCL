e3b52045edb24e8564e3f8b83a27de72018cbe6a
Kat Sullivan <kmsullivan012@gmail.com>
2015-08-28 13:04:11 -0400

    added opencv, and shape detections files

---
+++ xcode/Shape.cpp
@@ -0,0 +1,9 @@
+//
+//  Shape.cpp
+//  UserApp
+//
+//  Created by Kathleen Sullivan on 8/28/15.
+//
+//
+
+#include "Shape.h"
---
+++ xcode/Shape.h
@@ -0,0 +1,14 @@
+//
+//  Shape.h
+//  UserApp
+//
+//  Created by Kathleen Sullivan on 8/28/15.
+//
+//
+
+#ifndef __UserApp__Shape__
+#define __UserApp__Shape__
+
+#include <stdio.h>
+
+#endif /* defined(__UserApp__Shape__) */
